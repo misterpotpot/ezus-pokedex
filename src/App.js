2f6b15d9981ffc070ada317c0fd80244bf41d15e
Jérémy Roche <jeremy@imotep.io>
2021-10-20 23:12:49 +0200

    fix syntax

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from "react";
 import PokeCard from "./components/PokeCard";
 import PokeList from "./components/PokeList";
 
-const App = (porps) => {
+const App = (props) => {
   const [listData, setListData] = useState([]);
-  const [cardData, setCardData] = useState({});
   useEffect(() => {
     const url = "https://pokeapi.co/api/v2/pokemon?limit=1138";
     fetch(url)
@@ -16,7 +15,7 @@ const App = (porps) => {
     <>
       <h1>Pokedex</h1>
       <PokeList data={listData} />
-      <PokeCard data={cardData} />
+      <PokeCard data={{}} />
     </>
   );
 };
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 const PokeList = (props) => {
   const { data } = props;
