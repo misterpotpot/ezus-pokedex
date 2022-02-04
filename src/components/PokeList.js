09664c7ed24b606af9a540959f575719b3d8f115
Jérémy Roche <jeremy@imotep.io>
2022-02-04 17:51:48 +0100

    Ticket 2 - 1

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,19 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const PokeList = (props) => {
   const { data } = props;
   const [searchTerm, setSearchTerm] = useState("");
-  const filtredData = data.filter((d) =>
-    d.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const [orderMode, setOrderMode] = useState(0);
+  const [filtredData, setFiltredData] = useState([]);
+
+  useEffect(() => {
+    setFiltredData(
+      data
+        .filter((d) => d.name.toLowerCase().includes(searchTerm.toLowerCase()))
+        .sort((a, b) =>
+          orderMode === 1
+            ? a.name.toLowerCase() - b.name.toLowerCase()
+            : orderMode === -1
+            ? b.name.toLowerCase() - a.name.toLowerCase()
+            : 0
+        )
+    );
+  }, [data, searchTerm, orderMode]);
 
   return (
     <div>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button
+          onClick={() => setOrderMode(orderMode >= 1 ? -1 : orderMode + 1)}
+        >
+          {orderMode === 1 ? "Tri AZ" : orderMode === -1 ? "Tri ZA" : "Tri"}
+        </button>
+      </div>
       <ul>
         {filtredData?.length
           ? filtredData.map((d, i) => <li key={i}>{d.name.toUpperCase()}</li>)
