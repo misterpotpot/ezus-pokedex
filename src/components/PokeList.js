ec605f02fdd2293aaddafbea98e0102f7550d75e
Jérémy Roche <jeremy@imotep.io>
2022-02-04 17:34:43 +0100

    Ticket 1

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PokeList = (props) => {
   const { data } = props;
+  const [searchTerm, setSearchTerm] = useState("");
+  const filtredData = data.filter((d) =>
+    d.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
       <ul>
-        {data.map((d, i) => (
-          <li key={i}>{d.name.toUpperCase()}</li>
-        ))}
+        {filtredData?.length
+          ? filtredData.map((d, i) => <li key={i}>{d.name.toUpperCase()}</li>)
+          : "Il n'y a aucun Pokemon qui correspond à cotre recherche"}
       </ul>
     </div>
   );
